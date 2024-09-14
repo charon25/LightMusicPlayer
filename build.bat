7e845d6bb76e316aa4cef1ca642172223a801782
Paul Kern <paul.kern.fr@gmail.com>
2024-09-14 19:38:19 +0200

    build script

---
+++ build.bat
@@ -0,0 +1,5 @@
+pyinstaller --onefile -i icon.png -n MusicPlayer --noconsole main.py
+copy icon.png dist\icon.png
+copy next.png dist\next.png
+copy pause.png dist\pause.png
+copy play.png dist\play.png
